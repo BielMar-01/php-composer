d6c20b6c3c7b14b640f27b45f050f0a068460c29
Gabriel Marques <bielmarques922@gmail.com>
2022-11-17 18:57:21 -0300

    PHP Composer - Modulo 03 - COnfigurando a PSR-4

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -36,6 +36,10 @@ class ComposerStaticInit86889a18232b51d60ab3ba00cb93450b
             'GuzzleHttp\\Promise\\' => 19,
             'GuzzleHttp\\' => 11,
         ),
+        'A' => 
+        array (
+            'Alura\\BuscadorDeCursos\\' => 23,
+        ),
     );
 
     public static $prefixDirsPsr4 = array (
@@ -79,6 +83,10 @@ class ComposerStaticInit86889a18232b51d60ab3ba00cb93450b
         'GuzzleHttp\\' => 
         array (
             0 => __DIR__ . '/..' . '/guzzlehttp/guzzle/src',
+        ),
+        'Alura\\BuscadorDeCursos\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/src',
         ),
     );
 
